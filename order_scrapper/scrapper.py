1d919828f464c7379b9f9e9913f76d86d558cc8a
Vinay Pant <vinaypant24@gmail.com>
2024-07-27 21:06:08 +0530

    UI Completed - tkinter

---
+++ order_scrapper/scrapper.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+
+
+from tqdm import tqdm
+
+
+
+class Scrapper_selenium():
+    
+    pass
