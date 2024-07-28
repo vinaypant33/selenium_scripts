023c3d11566e1570e65bb153a7e0a7c9558ed4cf
Vinay Pant <vinaypant24@gmail.com>
2024-07-28 15:11:19 +0530

    Single Thread added for UI

--- order_scrapper/scrapper.py
+++ order_scrapper/scrapper.py
@@ -10,4 +10,13 @@ from tqdm import tqdm
 
 class Scrapper_selenium():
     
-    pass
+    def __init__(self , username  = '' , password  = '') -> None:
+        self.main_browser  = webdriver.Chrome()
+        self.main_browser.get('https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0')
+        
+        
+    def visit_website(self):
+        self.main_browser.get('https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0')
+        
+        
+        
--- order_scrapper/tkinter_ui.py
+++ order_scrapper/tkinter_ui.py
@@ -1,89 +1,113 @@
 import tkinter as tk
 from tkinter import ttk
-
 from pubsub import pub
 from time import sleep
-
-
-main_application  = tk.Tk()
-main_application.title("Amazon Order Scrapper")
-app_width  = 400 
-app_height  = 200
-
-
-x_location  = ( main_application.winfo_screenwidth() //2 ) - ( app_width //2 )
-y_location  = (main_application.winfo_screenheight() //2 ) - (app_height // 2)
-main_application.geometry(f'{app_width}x{app_height}+{x_location}+{y_location}')
-main_application.resizable( 0 , 0 )
-
-
-
-# Defining Contants : 
-yearly_selectionvar  = tk.BooleanVar()
-username_passwordvar  = tk.BooleanVar()
-is_working  = False
-
-
-def start_data_scrapping():
-    print("Data Scrapping Started")
-
-def stop_data_scrapping():
-    print("Data Scrapping Stopped")
+import threading
 
 
 
 
-# Funcations that are to be added in the main application : 
-def username_password():
-    
-    print(username_passwordvar.get())
-    if username_passwordvar.get() == True:
-        # Controls for the username and password  : 
-        global username_textbox
-        global password_textbox
-        username_textbox  = ttk.Entry(main_application , width=40 )
-        password_textbox  = ttk.Entry(main_application  , width=40)
+from scrapper import Scrapper_selenium
+
+
+
+
+
+def single_threaded_ui():
         
+
+    main_application  = tk.Tk()
+    main_application.title("Amazon Order Scrapper")
+    app_width  = 400 
+    app_height  = 200
+
+
+    x_location  = ( main_application.winfo_screenwidth() //2 ) - ( app_width //2 )
+    y_location  = (main_application.winfo_screenheight() //2 ) - (app_height // 2)
+    main_application.geometry(f'{app_width}x{app_height}+{x_location}+{y_location}')
+    main_application.resizable( 0 , 0 )
+
+
+
+    # Defining Contants : 
+    yearly_selectionvar  = tk.BooleanVar()
+    username_passwordvar  = tk.BooleanVar()
+    is_working  = False
+
+
+
+
+    def start_data_scrapping():
+        webdriver  = Scrapper_selenium()
+        webdriver.visit_website()
         
-        username_textbox.insert(0 , "Enter Username")
-        password_textbox.insert( 0 , "Enter Password")
+
+    def stop_data_scrapping():
+        print("Data Scrapping Stopped")
+
+
+
+
+    # Funcations that are to be added in the main application : 
+    def username_password():
         
-        username_textbox.bind("<FocusIn>" , lambda e : username_textbox.delete( 0 , 'end'))
-        password_textbox.bind("<FocusIn>" , lambda e : password_textbox.delete(0  , "end"))
-        
-        password_textbox.bind("<KeyRelease>" , lambda e : password_textbox.configure(show="*"))
-        username_textbox.place(x = 10 , y  = 120)
-        password_textbox.place(x = 10 , y = 150)
-    else:
-        try:
-            username_textbox.destroy()
-            password_textbox.destroy()
-        except Exception as error:
-            print(error)
+        print(username_passwordvar.get())
+        if username_passwordvar.get() == True:
+            # Controls for the username and password  : 
+            global username_textbox
+            global password_textbox
+            username_textbox  = ttk.Entry(main_application , width=40 )
+            password_textbox  = ttk.Entry(main_application  , width=40)
+            
+            
+            username_textbox.insert(0 , "Enter Username")
+            password_textbox.insert( 0 , "Enter Password")
+            
+            username_textbox.bind("<FocusIn>" , lambda e : username_textbox.delete( 0 , 'end'))
+            password_textbox.bind("<FocusIn>" , lambda e : password_textbox.delete(0  , "end"))
+            
+            password_textbox.bind("<KeyRelease>" , lambda e : password_textbox.configure(show="*"))
+            username_textbox.place(x = 10 , y  = 120)
+            password_textbox.place(x = 10 , y = 150)
+        else:
+            try:
+                username_textbox.destroy()
+                password_textbox.destroy()
+            except Exception as error:
+                print(error)
 
 
 
-start_button  = ttk.Button(main_application  , text="Start Scrapping", width=30 , command=start_data_scrapping)
-stop_button = ttk.Button(main_application  , text="Stop Scrapping" , width=30 , command=stop_data_scrapping)
+    start_button  = ttk.Button(main_application  , text="Start Scrapping", width=30 , command=start_data_scrapping)
+    stop_button = ttk.Button(main_application  , text="Stop Scrapping" , width=30 , command=stop_data_scrapping)
 
-# Save data by each year : 
-save_label  = ttk.Label(main_application  , text="Save Data on Yearly Basis : ")
-selction_button  =  ttk.Checkbutton(main_application , text="Save Data on yearly basis : " , variable=yearly_selectionvar)
+    # Save data by each year : 
+    save_label  = ttk.Label(main_application  , text="Save Data on Yearly Basis : ")
+    selction_button  =  ttk.Checkbutton(main_application , text="Save Data on yearly basis : " , variable=yearly_selectionvar)
 
-username_password_select  = ttk.Checkbutton(main_application , text="Automatically Enter Username Password" , variable=username_passwordvar , command=username_password)
-
-
-sleep_label  = ttk.Label(main_application , text="Enter Delay Timer : ")
-sleep_timer  = ttk.Spinbox(main_application  , width=10 , from_=0 , to=10)
+    username_password_select  = ttk.Checkbutton(main_application , text="Automatically Enter Username Password" , variable=username_passwordvar , command=username_password)
 
 
+    sleep_label  = ttk.Label(main_application , text="Enter Delay Timer : ")
+    sleep_timer  = ttk.Spinbox(main_application  , width=10 , from_=0 , to=10)
 
-start_button.place(x = 10 , y = 10)
-stop_button.place(x = 205 , y = 10)
-# save_label.place(x = 10 , y = 40)
-selction_button.place(x = 10 , y = 50)
-username_password_select.place(x = 10 , y = 70)
-sleep_label.place(x = 205 , y = 45)
-sleep_timer.place(x = 310 , y = 45)
 
-main_application.mainloop()
+
+    start_button.place(x = 10 , y = 10)
+    stop_button.place(x = 205 , y = 10)
+    # save_label.place(x = 10 , y = 40)
+    selction_button.place(x = 10 , y = 50)
+    username_password_select.place(x = 10 , y = 70)
+    sleep_label.place(x = 205 , y = 45)
+    sleep_timer.place(x = 310 , y = 45)
+
+    main_application.mainloop()
+
+
+main_thread  = threading.Thread(target=single_threaded_ui)
+
+
+main_thread.start()
+
+
+print("threading part done")
