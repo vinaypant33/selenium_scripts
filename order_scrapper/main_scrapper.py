a2ddaebab65b9b15535c32fe1ff8f14a3b603364
Vinay Pant <vinaypant24@gmail.com>
2024-08-02 03:06:11 +0530

    Base Scrapping Completed

---
+++ order_scrapper/main_scrapper.py
@@ -0,0 +1,270 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import Select
+# from time import sleep
+# from tqdm import tqdm
+
+# # For temporary solution : Closing automated browser  : 
+# import pyautogui
+
+# '''
+# # Press and hold the first key
+# pyautogui.keyDown('ctrl')
+
+# # Press and hold the second key
+# pyautogui.keyDown('c')
+
+# # Release the second key
+# pyautogui.keyUp('c')
+
+# # Release the first key
+# pyautogui.keyUp('ctrl')
+# '''
+
+# class Selenium_scrapper():
+    
+    
+#     def csv_saving(self):
+#         print("Data")
+    
+    
+#     def __init__(self , username  = '' , password = '' , delay = 6  , yearly_selection   = False , autoamted_username  = False) -> None:
+#         self.delay = delay
+#         self.username  = username
+#         self.password = password
+#         self.yearly_selection  = yearly_selection
+#         self.automated_username  = autoamted_username
+        
+        
+#         options = webdriver.ChromeOptions()
+#         options.add_experimental_option("detach", True)
+#         self.chrome_driver = webdriver.Chrome(options = options)
+
+#         # pyautogui.keyDown('ctrl')
+#         # pyautogui.keyDown('w')
+#         # pyautogui.keyUp('ctrl')
+#         # pyautogui.keyUp('w')
+        
+#         self.chrome_driver.maximize_window()
+        
+        
+        
+#         self.chrome_driver.get(r"https://www.amazon.in/ap/signin?openid.pape.max_auth_age=900&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fyourstore%2Fhome%3Fpath%3D%252Fgp%252Fyourstore%252Fhome%26signIn%3D1%26useRedirectOnSuccess%3D1%26action%3Dsign-out%26ref_%3Dnav_AccountFlyout_signout&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
+#         sleep(self.delay)
+        
+        
+#         if self.username == '':
+#             sleep(self.delay * 2)
+         
+#             current_title = self.chrome_driver.title
+#             print(current_title)
+#             if current_title == 'Amazon Sign In':
+#                 print("Contains")
+#             else:
+#                 pass
+#             self.chrome_driver.get('https://www.amazon.in/your-orders/orders?timeFilter=year-2024&ref_=ppx_yo2ov_dt_b_filter_all_y2024')
+            
+           
+#         else:
+#             try:
+                
+#                 current_username_selection  = self.chrome_driver.find_element(By.NAME , "email")
+#                 current_username_selection.send_keys(self.username)
+#                 continue_button = self.chrome_driver.find_element(By.ID , 'continue').click()
+#                 current_password_selection  = self.chrome_driver.find_element(By.NAME , 'password')
+#                 current_password_selection.send_keys(self.password)
+#                 password_button  = self.chrome_driver.find_element(By.ID , 'signInSubmit').click()
+#                 sleep(self.delay * 2)
+#             except Exception as login_error:
+#                 print(login_error)
+#                 print('Unable to Login , waiting for Delay to login ')
+#                 sleep(self.delay * 3)
+                
+#             current_number = 2024
+#             for i in range(15):
+#                 try:
+#                     self.chrome_driver.get(f'https://www.amazon.in/your-orders/orders?timeFilter=year-{current_number}')
+#                     current_title = self.chrome_driver.title
+#                     if current_title == 'Your Orders':
+#                         try:
+#                             for i in range(15):
+#                                 self.chrome_driver.get(f'https://www.amazon.in/your-orders/orders?timeFilter=year-{current_number}&startIndex={i * 10}')
+#                                 sleep(self.delay)
+#                                 print("i have reached here ")
+                                
+#                                 try:
+#                                     current_amount  = self.chrome_driver.find_elements(By.CLASS_NAME , "a-color-secondary value") #a-size-base a-color-secondary   a-size-base a-color-secondary
+#                                 except Exception as first_error:
+#                                     print(first_error)
+                                
+#                                 try:
+#                                     current_amount  = self.chrome_driver.find_elements(By.CLASS_NAME , 'a-size-base a-color-secondary')
+#                                 except Exception as second_error:
+#                                     print(second_error)
+                                
+#                                 for each in current_amount:
+#                                     print("reached and corected ")
+#                                     print(each)
+#                                     print(each.get_attribute('value'))
+#                         except Exception as page_error:
+#                             print(f"Pagenation error {page_error}")
+#                     current_number-=1
+#                     sleep(self.delay // 2)
+#                 except Exception as main_error:
+#                     print(main_error)
+#                     return
+            
+            
+    
+        
+#     def close_browser(self):
+#         self.chrome_driver.close()
+#         self.chrome_driver.quit()
+
+
+# if __name__ == '__main__':
+#     main_driver  = Selenium_scrapper()
+
+
+from typing import Any
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from time import sleep
+from tqdm import tqdm
+
+# For Reference: 
+import pyautogui
+
+import csv
+
+
+class Scrapper():
+    
+    
+    def __init__(self , username = '' , password = '' , delay = 6) -> None:
+        self.delay = delay
+        self.username  = 'vinaypant24@gmail.com'
+        self.password = 'Stillconquering@2290'
+        
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('detach' , True)
+        self.chrome_driver = webdriver.Chrome(options=options)
+        
+        self.chrome_driver.maximize_window()
+        
+        self.chrome_driver.get(r"https://www.amazon.in/ap/signin?openid.pape.max_auth_age=900&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fyourstore%2Fhome%3Fpath%3D%252Fgp%252Fyourstore%252Fhome%26signIn%3D1%26useRedirectOnSuccess%3D1%26action%3Dsign-out%26ref_%3Dnav_AccountFlyout_signout&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0")
+        sleep(self.delay)
+        
+        print(self.username)
+        if self.username == '':
+            pass
+        else:
+            print(self.chrome_driver.title)
+            try:
+                current_username_selection  = self.chrome_driver.find_element(By.NAME , "email")
+                current_username_selection.send_keys(self.username)
+                continue_button = self.chrome_driver.find_element(By.ID , 'continue').click()
+                current_password_selection  = self.chrome_driver.find_element(By.NAME , 'password')
+                current_password_selection.send_keys(self.password)
+                password_button  = self.chrome_driver.find_element(By.ID , 'signInSubmit').click()
+                sleep(self.delay * 2)
+                print(self.chrome_driver.title)
+            except Exception as login_error:
+                print(login_error)
+                print('Unable to Login , waiting for Delay to login ')
+                sleep(self.delay * 3)
+            
+            
+            current_number = 2024
+            for i in range(15):
+                try:
+                    self.chrome_driver.get(f'https://www.amazon.in/your-orders/orders?timeFilter=year-{current_number}')
+                    current_title = self.chrome_driver.title
+                    if current_title == 'Your Orders':
+                        try:
+                            for i in range(15):
+                                self.chrome_driver.get(f'https://www.amazon.in/your-orders/orders?timeFilter=year-{current_number}&startIndex={i * 10}')
+                                sleep(self.delay // 2)
+                                
+                                
+                                
+                                base_element  = self.chrome_driver.find_elements(By.CLASS_NAME , "a-box-group a-spacing-base order js-order-card")
+                                
+                                
+                                for each in base_element:
+                                    
+                                    print('element scrapped but inner text not working')
+                                    first_class_element = self.chrome_driver.find_element(By.CLASS_NAME , "a-color-secondary value")
+                                    
+                                    second_class_element  = self.chrome_driver.find_element(By.CLASS_NAME , "currencyINRFallback")
+                                    
+                                    
+                                    print(each)
+                                    
+                                    
+                                    
+                                    
+                                    
+                               
+                                # price_data  = self.chrome_driver.find_elements(By.CLASS_NAME , 'yohtmlc-product-title')
+                                # # price_data  = self.chrome_driver.find_elements(By.XPATH , '//*[@id="a-page"]/section/div[1]/div[15]/div/div[1]/div/div/div/div[1]/div/div[2]/div[2]/span')
+                                
+                                # # self.csv_saving(price_data)
+                                # for element in price_data:
+                                #     self.csv_saving(element.text)
+
+                                # try:
+                                #     current_amount  = self.chrome_driver.find_elements(By.CLASS_NAME , "a-color-secondary value") #a-size-base a-color-secondary   a-size-base a-color-secondary
+                                # except Exception as first_error:
+                                #     print(first_error)
+                                
+                                # try:
+                                #     current_amount  = self.chrome_driver.find_elements(By.CLASS_NAME , 'a-size-base a-color-secondary')
+                                # except Exception as second_error:
+                                #     print(second_error)
+                                
+                                # for each in current_amount:
+                                #     print("reached and corected ")
+                                #     print(each)
+                                #     print(each.get_attribute('value'))
+                        except Exception as page_error:
+                            print(f"Pagenation error {page_error}")
+                    current_number-=1
+                    sleep(self.delay // 2)
+                except Exception as main_error:
+                    print(main_error)
+                    return
+    
+    def csv_saving(self , element):
+       
+        
+        try:
+            with open('scrapped.csv' , mode='a' ,newline='') as file:
+                csv_writer = csv.writer(file)
+                csv_writer.writerow([element])
+        except Exception as error:
+            print(error)
+            
+            
+'''
+
+order-card js-order-card
+
+
+
+a-box-group a-spacing-base order js-order-card
+
+
+currencyINRFallback
+
+a-color-secondary value
+
+a-link-normal
+
+
+
+
+'''
--- order_scrapper/ttkbootstrap_ui.py
+++ order_scrapper/ttkbootstrap_ui.py
@@ -2,32 +2,59 @@ import ttkbootstrap as btk
 import threading
 
 
-import selenium_scrapper_2
-
-
+from main_scrapper import Scrapper
 
 
 def ui():
     
+    def thread_started():
+        print(current_username)
+        webdriver  = Scrapper(username=current_username , password=current_password)
+        # webdriver = Selenium_scrapper()
+        
+        
+        
+    def checking_():
+        global is_working
+        current_status  = is_working
+        if is_working == True:
+            pass
+        else:
+            return
+        main_window.after(1000 , checking_)
     
     def data_scrapping():
-        pass
+        global thread_count
+        thread_count+=1
+        if thread_count <= 1:
+            scrapping_thread  = threading.Thread(target=thread_started)
+            scrapping_thread.start()
+            scrapping_thread.join()
+        else:
+            print("thread already started")
+            
+    
+    def stop_scrapping():
+        global is_working
+        is_working = False
     
     def checkbox_checked():
         if unpwd_const.get() ==1 :
-            print("1")
             main_window.geometry("300x225")
             username_entry = btk.Entry(main_window  , width=45)
             password_entry = btk.Entry(main_window , show="*" , width=45)
-            
             username_entry.insert( 0 , "vinaypant24@gmail.com")
-            password_entry.insert(0 , "Stillconquering@229")
-            
+            password_entry.insert(0 , "Stillconquering@2290")
             username_entry.place(x = 10 , y = 155)
             password_entry.place(x = 10 , y = 185)
             
+            global current_username
+            global current_password
+            
+            current_password = username_entry.get()
+            current_password = password_entry.get()
+            
         else:
-            print("0")
             main_window.geometry("300x155")
     
     
@@ -36,6 +63,18 @@ def ui():
     #constants for the username and password
     unpwd_const  = btk.IntVar()
     unpwd_const.set(0)
+    
+    global is_working
+    is_working  = True
+    
+    global thread_count
+    thread_count  = 0
+    
+    global current_username
+    global current_password
+    
+    current_username = ''
+    current_password  = ''
     
     
     main_window.title("Amazon Scrapper")
@@ -48,10 +87,8 @@ def ui():
     sleep_label = btk.Label(main_window , text="Select Wait Time")
     sleep_selector  = btk.Spinbox(main_window , from_=1 , to=20)
     auto_selector_unpwd = btk.Checkbutton(main_window , text="Auto Enter Username and Password"  ,variable=unpwd_const , command=checkbox_checked)
-    start_button  = btk.Button(main_window , text="Start Scrapping" , width=18)
-    stop_button = btk.Button(main_window , text="Stop Scrapping" , width=18)
-    
-    
+    start_button  = btk.Button(main_window , text="Start Scrapping" , width=18 , command=data_scrapping)
+    stop_button = btk.Button(main_window , text="Stop Scrapping" , width=18 , command=stop_scrapping)
     
     
     depth_selector.set(5)
@@ -67,6 +104,7 @@ def ui():
     auto_selector_unpwd.place(x = 10 , y = 90)
     start_button.place(x =10 , y = 120)
     stop_button.place(x = 160 , y = 120)
+    checking_()
     main_window.mainloop()
 
 
